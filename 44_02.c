3dd7824e3dc9cfe7907d083426f6c045b3936236
Naveen Kushwaha <naveen17593@gmail.com>
2026-06-04 16:53:19 +0530

    A44 02

---
+++ 44_02.c
@@ -0,0 +1,25 @@
+/*
+Assignment : 44
+Topic : Structure
+02||       Write a funcion to take input employee data from the user [Refer structure from question 1].
+
+*/
+#include<stdio.h>
+struct Employee
+{
+    int memberid, salary;
+    char name[30];
+};
+int main()   
+{
+    struct Employee E1;
+    printf("Enter Name: ");
+    fgets(E1.name, 30, stdin);
+    printf("Enter Member Id & Salary of an Employee: ");
+    scanf("%d %d",&E1.memberid, &E1.salary);
+    for ( int i = 0 ; E1.name[i] != '\0' ; i++)
+        if (E1.name[i] == '\n')
+            E1.name[i] = '\0';
+    printf("A Employee whose name %s, member id (%d) with a salary of %d", E1.name, E1.memberid, E1.salary);
+    return 0;
+}
