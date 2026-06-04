2e8fcad75109b3965d7ff85d784d0fd231b428ea
Naveen Kushwaha <naveen17593@gmail.com>
2026-06-04 16:53:05 +0530

    A44 01

---
+++ 44_01.c
@@ -0,0 +1,12 @@
+/*
+Assignment : 44
+Topic : Structure
+01||       Define a structure Employee with member variables id, name, salary.
+
+*/
+#include<stdio.h>
+struct Employee
+{
+    int memberid, name, salary;
+};
+   
